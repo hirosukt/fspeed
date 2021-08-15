0dd957a1bcc045ff63849ce2c7a5e661f423906b
BuildTools <unconfigured@null.spigotmc.org>
2021-08-16 00:39:55 +0900

    test build

--- src/main/java/hirosuke/fspeed/Fspeed.java
+++ src/main/java/hirosuke/fspeed/Fspeed.java
@@ -15,7 +15,7 @@ public final class Fspeed extends JavaPlugin implements CommandExecutor {
 
     @Override
     public void onDisable() {
-        getLogger().info("plugin has unloadeda.");
+        getLogger().info("plugin has unloaded.");
     }
 
     @Override
