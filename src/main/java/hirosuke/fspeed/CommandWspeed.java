23c077d0585ad29f3349249a476cddd18eec5e2b
Hirosuke <senolynn7584@gmail.com>
2021-09-14 22:47:28 +0900

    remove api version setting

--- src/main/java/hirosuke/fspeed/CommandWspeed.java
+++ src/main/java/hirosuke/fspeed/CommandWspeed.java
@@ -1,5 +1,6 @@
 package hirosuke.fspeed;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -10,7 +11,12 @@ public class CommandWspeed implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(command.getName().equalsIgnoreCase("wspeed") || command.getName().equalsIgnoreCase("ws")) {
             if(args.length != 0 && sender instanceof Player) {
-                Player player = ((Player) sender).getPlayer();
+                Player player;
+                if(args.length != 2) {
+                    player = (Player) sender;
+                } else {
+                    player = Bukkit.getPlayer(args[1]);
+                }
                 try {
                     float amount = Float.parseFloat(args[0]);
                     player.setWalkSpeed(amount);
